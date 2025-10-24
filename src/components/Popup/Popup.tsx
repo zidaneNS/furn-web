02dadf53f333a44cfcd10cdea1bb1d09a5e9790f
zidaneNS <zidane.nur004@gmail.com>
2025-10-24 20:24:35 +0700

    adjust popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -2,7 +2,7 @@ import dateRange from '../../assets/icons/icon-date-range.svg';
 import timeIcon from '../../assets/icons/icon-time.svg';
 import groupIcon from '../../assets/icons/icon-group.svg';
 import expandDown from '../../assets/icons/icon-expand-down.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Popup.css';
 import { ConfigProvider, DatePicker, Select, TimePicker } from 'antd';
 import dayjs from 'dayjs';
@@ -25,6 +25,22 @@ const steps: Step[] = [
 
 export default function Popup() {
   const [selectedStep, setSelectedStep] = useState<Step>(steps[0]);
+  const [showAvailableTime, setShowAvailableTime] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    
+    setIsLoading(true)
+    const time = setTimeout(() => {
+      if (showAvailableTime) {
+        setIsLoading(false)
+      }
+    }, 1000)
+
+    return () => clearTimeout(time);
+
+  }, [showAvailableTime]);
+
   return (
     <div className="popup">
       <div className="step-container">
@@ -72,13 +88,17 @@ export default function Popup() {
               />
             </ConfigProvider>
           </div>
-          <div className="cta-button" style={{ width: '100%' }}>Find a Table</div>
+          <div onClick={() => setShowAvailableTime(true)} className="cta-button" style={{ width: '100%' }}>Find a Table</div>
 
-          <div className="available-schedules-container">
-            {Array.from({ length: 5 }).map((_, id) => (
-              <div className="available-shcedule cta-button" key={id} onClick={() => setSelectedStep(steps[1])}>05:30 pm</div>
-            ))}
-          </div>
+          {showAvailableTime && isLoading && <p>Loading...</p>}
+
+          {showAvailableTime && !isLoading && (
+            <div className="available-schedules-container">
+              {Array.from({ length: 7 }).map((_, id) => (
+                <div className="available-shcedule cta-button" key={id} onClick={() => setSelectedStep(steps[1])}>05:30 pm</div>
+              ))}
+            </div>
+          )}
         </div>
       )}
 
